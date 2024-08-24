db8f9f2157e92b12a2dfa3190ea3072eb5b00fe9
Praveen123445 <Pravin324@gmail.com>
2024-08-24 20:34:31 +0530

    first commit

---
+++ app/page.jsx
@@ -0,0 +1,8 @@
+import UserInfo from "@/components/UserInfo";
+import Image from "next/image";
+
+export default function Home() {
+  return <div className="grid place-items-center h-screen -mt-24 ">
+    <UserInfo/>
+  </div>
+}
